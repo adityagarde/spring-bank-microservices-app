93055b531fe3f0c2b7159aa1b96a51b07c2589d9
adityagarde <aditya.garde96@gmail.com>
2022-01-15 01:37:31 +0530

    Adding @SuperBuilder, build failure

--- cqrs-es/cqrs.core/src/main/java/com/github/adityagarde/cqrs/core/messages/Message.java
+++ cqrs-es/cqrs.core/src/main/java/com/github/adityagarde/cqrs/core/messages/Message.java
@@ -3,10 +3,12 @@ package com.github.adityagarde.cqrs.core.messages;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public abstract class Message {
     private String id;
 }
