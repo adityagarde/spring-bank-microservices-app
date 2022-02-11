df867e2f0988f522740819050737e01bc257494c
adityagarde <aditya.garde96@gmail.com>
2022-02-12 02:53:37 +0530

    Bug Fix - Adding @NoArgsConstructor

--- bank-account/account.common/src/main/java/com/github/adityagarde/account/common/events/AccountClosedEvent.java
+++ bank-account/account.common/src/main/java/com/github/adityagarde/account/common/events/AccountClosedEvent.java
@@ -3,11 +3,13 @@ package com.github.adityagarde.account.common.events;
 import com.github.adityagarde.cqrs.core.events.BaseEvent;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class AccountClosedEvent extends BaseEvent {
 }
